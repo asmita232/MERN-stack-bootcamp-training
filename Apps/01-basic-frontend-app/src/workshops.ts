f8e31f828a27d7d51cda2d70dba1efc609b0137b
Chavan <Asmita.Chavan@team.telstra.com>
2020-09-08 22:13:55 +0530

    This folder containes all the apps

---
+++ Apps/01-basic-frontend-app/src/workshops.ts
@@ -0,0 +1,32 @@
+// import workshops from './services/workshops.js'
+// workshops()
+
+
+import IWorkshop from './models/IWorkshop'
+
+export default function workshops(){
+    fetch( `http://workshops-server.herokuapp.com/workshops` )
+    .then(function( response: Response ) {
+        return response.json();
+    })
+    .then(function( workshops: IWorkshop[] ) {
+        console.log( workshops );
+        
+        const tbody = <HTMLElement> document.querySelector( 'tbody' );
+        
+        // go through workshops array and form HTML within tbody
+        workshops.forEach(function( workshop : IWorkshop ) {
+            tbody.innerHTML += `<tr>
+                <td>
+                    <img src="${workshop.imageUrl}" style="width: 200px;" />
+                </td>
+                <td>${workshop.name}</td>
+                <td>${workshop.location.city}</td>
+            </tr>`;
+        });
+    })
+    .catch(function( error ) {
+        console.log( error.message );
+    });
+}
+
